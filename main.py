9da3292afca1d179512937e12460c3e7a9a2384b
hayawi301 <49540449+hayawi301@users.noreply.github.com>
2026-09-05 07:34:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print(f'البوت اشتغل وصار أونلاين باسم: {client.user}')
+    print(f'البوت اشتغل وصار أونلاين باسم {client.user}')
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content == '!مرحبا':
-        await message.channel.send('أهلاً بك يحيى! البوت يعمل بنجاح 🚀')
+    if message.content == '!مرطبا!':
+        await message.channel.send('عمل بنجاح')
 
 client.run('MTU0NTYyMzQ0MzU3MDA0MDk3Mg.GxMClh.UifW-DUzKMphLDQA2jZWRkr8l43y6BhcOujhMw')
